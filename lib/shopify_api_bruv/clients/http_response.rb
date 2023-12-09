759a12986379a018b33455232a022b40b0cb59ed
ident <korewaident@gmail.com>
2023-12-09 20:47:26 +0100

    add page info to http response

--- lib/shopify_api_bruv/clients/http_response.rb
+++ lib/shopify_api_bruv/clients/http_response.rb
@@ -3,12 +3,13 @@
 module ShopifyApiBruv
   module Clients
     class HttpResponse
-      attr_reader :code, :headers, :body
+      attr_reader :code, :headers, :body, :page_info
 
       def initialize(code:, headers:, body:)
         @code = code
         @headers = headers
         @body = body
+        @page_info = parse_page_info
 
         ShopifyApiBruv.logger(
           method: :info,
@@ -19,6 +20,26 @@ module ShopifyApiBruv
       end
 
       private
+
+      def parse_page_info
+        return { previous: nil, next: nil } if headers['link'].nil?
+
+        page_info = {}
+
+        headers['link'].split(',').each do |link|
+          rel = link.match(/rel="(.*?)"/).captures.first
+          url = link.match(/<(.*?)>/).captures.first
+
+          URI.parse(url).query.split('&').each do |param|
+            if param.split('=').first == 'page_info'
+              page_info[rel] = param.split('=').last
+              break
+            end
+          end
+        end
+
+        { previous: page_info['previous'], next: page_info['next'] }
+      end
 
       def ok?
         (200..299).cover?(code)
